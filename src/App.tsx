38715d137cf4fe41627ef4f38f7eb4ea41789975
mariappan9524 <vineethp117@gmail.com>
2026-07-08 12:46:02 +0530

    optimization

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,66 @@
+import {
+  lazy,
+  Suspense,
+} from "react";
+
 import { AnimatePresence } from "framer-motion";
 
 import Navbar from "./components/layout/Navbar";
 import AnimatedBackground from "./components/effects/AnimatedBackground";
 import ScrollProgress from "./components/effects/ScrollProgress";
-
 import LoadingScreen from "./components/loading/LoadingScreen";
+import BackToTop from "./components/effects/BackToTop";
 
 import useLoading from "./hooks/useLoading";
 
 import Hero from "./sections/Hero";
-import About from "./sections/About";
-import Skills from "./sections/Skills/Skills";
-import Projects from "./sections/Projects/Projects";
-import Experience from "./sections/Experience/Experience";
-import Contact from "./sections/Contact/Contact";
-import Footer from "./sections/Footer/Footer";
-import BackToTop from "./components/effects/BackToTop";
+
+// Lazy Loaded Sections
+const About = lazy(() => import("./sections/About"));
+const Skills = lazy(() => import("./sections/Skills/Skills"));
+const Projects = lazy(() => import("./sections/Projects/Projects"));
+const Experience = lazy(() => import("./sections/Experience/Experience"));
+const Contact = lazy(() => import("./sections/Contact/Contact"));
+const Footer = lazy(() => import("./sections/Footer/Footer"));
+
 function App() {
   const loading = useLoading();
 
   return (
-    <>
-      <AnimatePresence mode="wait">
-        {loading ? (
-          <LoadingScreen key="loading" />
-        ) : (
-          <main
-            key="portfolio"
-            className="
-              relative
-              z-10
-              overflow-x-hidden
-              text-white
-            "
-          >
-            <AnimatedBackground />
-            <ScrollProgress />
+    <AnimatePresence mode="wait">
+      {loading ? (
+        <LoadingScreen key="loading" />
+      ) : (
+        <main
+          key="portfolio"
+          className="
+            relative
+            z-10
+            overflow-x-hidden
+            text-white
+          "
+        >
+          <AnimatedBackground />
 
-            <Navbar />
+          <ScrollProgress />
 
-            <Hero />
+          <Navbar />
 
+          <Hero />
+
+          <Suspense fallback={null}>
             <About />
-
             <Skills />
-
             <Projects />
-
             <Experience />
-
             <Contact />
-
             <Footer />
-            <BackToTop />
-          </main>
-        )}
-      </AnimatePresence>
-    </>
+          </Suspense>
+
+          <BackToTop />
+        </main>
+      )}
+    </AnimatePresence>
   );
 }
 
